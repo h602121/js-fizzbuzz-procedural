641198dfa9ecc2caab65b0de7e41291f17778ad1
Øyvind Onarheim <oyvind.aasmul.onarheim@experisacademy.no>
2024-09-20 14:36:45 +0200

    done

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,7 +2,19 @@ const answer = []
 
 // Write your code below this line
 
-
+for (let index = 1; index < 16; index++) {
+    if(index % 3 == 0 && index % 5 == 0){
+        answer.push("FizzBuzz")
+    }
+    else if(index % 3 == 0){
+        answer.push("Fizz")
+    } else if(index % 5 == 0){
+        answer.push("Buzz")
+    } else {
+        answer.push(index)
+    }
+    
+}
 
 
 
